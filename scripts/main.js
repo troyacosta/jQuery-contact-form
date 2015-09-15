5262f7fb4bccb6589f08893c28f8f8bb67a0c891
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-15 17:46:52 -0500

    push

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,36 @@
 $(document).ready(function() {
 
 var validate = function() {
+	var count = 0;
 	if ($('#nameInput').val() === '') {
 		$('#nameError').show();
 		$('#nameInput').css({'border-left': '.25em solid red'})
 	} 
+	else {
+		count++
+	}
 	if ($('#emailInput').val().indexOf('@') === -1) {
 		$('#emailError').show();
-		$('#nameInput').css({'border-left': '.25em solid red'})
+		$('#emailInput').css({'border-left': '.25em solid red'})
+	}
+	else {
+		count++
 	}
 	if ($('#websiteInput').val().substr(0, 7) !== 'http://') {
 		$('#websiteError').show();
-		$('#nameInput').css({'border-left': '.25em solid red'})
+		$('#websiteInput').css({'border-left': '.25em solid red'})
+	}
+	else {
+		count++
 	}
 	if ($('#textareaInput').val() === '') {
 		$('#textareaError').show();
-		$('#nameInput').css({'border-left': '.25em solid red'})
+		$('#textareaInput').css({'border-left': '.25em solid red'})
 	}
 	else {
+		count++
+	}
+	if (count === 4) {
 		$('form').hide();
 		$('#success').show();
 		$('#success').html('<h2>Thanks for contacting us ' + $('#nameInput').val() + '. We have received your message and will be in touch shortly.</h2>')
